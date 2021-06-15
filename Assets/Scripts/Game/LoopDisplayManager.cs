6d8383258456b17dfcbdf721071b59fc8255631c
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-06-15 16:51:10 -0700

    Render secondary beat lines

--- Assets/Scripts/Game/LoopDisplayManager.cs
+++ Assets/Scripts/Game/LoopDisplayManager.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 public class LoopDisplayManager : MonoBehaviour
 {
     const float RADIUS = 4f;
+    const float SUB_BEAT_LINE_WIDTH = 0.025f;
+
+    public Material lineMaterial;
 
     LineRenderer metronomeLine;
-
+    
     void Awake()
     {
         metronomeLine = transform.Find("Metronome Line").GetComponent<LineRenderer>();
     }
 
-    public void SetMetronome(float barPercent)
+    Vector3 CalcLinePosition(float barPercent)
     {
         float angle = (barPercent + 0.25f) * 2 * Mathf.PI;
         float x = RADIUS * Mathf.Cos(angle);
         float y = RADIUS * Mathf.Sin(angle);
-        metronomeLine.SetPosition(1, new Vector3(x, y, 0));
+        return new Vector3(x, y, 0);
+    }
+
+    public void SetMetronome(float barPercent)
+    {
+        metronomeLine.SetPosition(1, CalcLinePosition(barPercent));
+    }
+
+    public void MakeBeatLines(int beatsPerBar)
+    {
+        for (int i = 1; i < beatsPerBar; i++)
+        {
+            GameObject line = new GameObject("Beat " + (i + 1) + " Line");
+            line.transform.parent = transform;
+
+            LineRenderer lr = line.AddComponent<LineRenderer>();
+            lr.material = lineMaterial;
+            lr.sortingLayerName = "Track";
+            lr.sortingOrder = 100;
+            lr.startWidth = lr.endWidth = SUB_BEAT_LINE_WIDTH;
+            lr.SetPosition(1, CalcLinePosition((float)i / beatsPerBar));
+        }
     }
 }
